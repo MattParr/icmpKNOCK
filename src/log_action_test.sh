3ed22f47e7ba10ff7539b93bd1e1af7818afd3ef
Matt Parr <matthew.parr@itlab.com>
2015-04-18 14:47:19 +0100

    test for example action

---
+++ src/log_action_test.sh
@@ -0,0 +1,4 @@
+ping -c1 -p 'c7533ff6fabc19816a2d816941fa5f56' -I $1 $2
+ping -c1 -p 'fafe47632b2a5e0a8b5fe2e8406b970b' -I $1 $2
+ping -c1 -p 'f28dff849929fb5d43629328c23df1c1' -I $1 $2
+ping -c1 -p 'b96276ed3de61330494eb201a3cb7fa7' -I $1 $2
